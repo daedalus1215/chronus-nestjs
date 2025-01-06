8f66eaf1ed63bf4f4a883f134a733750eda21a55
la la <13687739+daedalus1215@users.noreply.github.com>
2025-01-05 23:43:16 -0500

    postgres: bring in HealthModule and postgres in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,36 @@
-// src/app.module.ts
-
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module'; // Import UsersModule
-import { AuthModule } from './auth/auth.module'; // Import AuthModule if required
-import { AppController } from './app/app.controller';
-import { AppService } from './domain/app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module"; // Import UsersModule
+import { AuthModule } from "./auth/auth.module"; // Import AuthModule if required
+import { AppController } from "./app/app.controller";
+import { AppService } from "./domain/app.service";
+import { HealthModule } from "./health/health.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`, // Use appropriate .env file
+      envFilePath: `.env.${process.env.NODE_ENV || "development"}`, // Use appropriate .env file
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService) => ({
-        type: 'sqlite',
-        database: 'db.sqlite',  // Path to your SQLite database file
-        entities: [__dirname + '/**/*.entity{.ts,.js}'], // Automatically load entities
+      useFactory: async (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DB_HOST") || "localhost",
+        port: configService.get<number>("DB_PORT") || 5432,
+        username: configService.get<string>("DATABASE_USER") || "admin",
+        password:
+          configService.get<string>("DATABASE_PASSWORD") || "adminpassword",
+        database: configService.get<string>("DATABASE_NAME") || "chronus",
+        entities: [__dirname + "/**/*.entity{.ts,.js}"], // Automatically load entities
         synchronize: true, // For development; disable in production
       }),
       inject: [ConfigService],
     }),
     UsersModule, // Add UsersModule
     AuthModule, // Include AuthModule if needed
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
