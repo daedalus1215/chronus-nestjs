9873d34dc1db70fa60951ed7e6d8b581c983110d
la la <13687739+daedalus1215@users.noreply.github.com>
2025-01-06 23:43:09 -0500

    postgres: move db out of app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./users/users.module"; // Import UsersModule
 import { AuthModule } from "./auth/auth.module"; // Import AuthModule if required
 import { AppController } from "./app/app.controller";
@@ -11,25 +10,10 @@ import { HealthModule } from "./health/health.module";
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV || "development"}`, // Use appropriate .env file
+      envFilePath: `.env`, 
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get<string>("DB_HOST") || "localhost",
-        port: configService.get<number>("DB_PORT") || 5432,
-        username: configService.get<string>("DATABASE_USER") || "admin",
-        password:
-          configService.get<string>("DATABASE_PASSWORD") || "adminpassword",
-        database: configService.get<string>("DATABASE_NAME") || "chronus",
-        entities: [__dirname + "/**/*.entity{.ts,.js}"], // Automatically load entities
-        synchronize: true, // For development; disable in production
-      }),
-      inject: [ConfigService],
-    }),
-    UsersModule, // Add UsersModule
-    AuthModule, // Include AuthModule if needed
+    UsersModule,
+    AuthModule,
     HealthModule,
   ],
   controllers: [AppController],
