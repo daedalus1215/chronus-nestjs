d73a7165d048bcbac66250deb7d59801166c5c12
la la <13687739+daedalus1215@users.noreply.github.com>
2025-01-25 16:02:40 -0500

    notes: Comment out the entities we are not ready for.

---
+++ src/notes/infra/schemas/notes/memo.entity.ts
@@ -0,0 +1,15 @@
+// import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+// import { Note } from './note.entity';
+
+// @Entity('memos')
+// export class Memo {
+//   @PrimaryGeneratedColumn()
+//   id: string;
+
+//   @Column('text')
+//   description: string;
+
+
+//   @OneToMany(() => Note, (note) => note.memo)
+//   notes: Note[];
+// }
