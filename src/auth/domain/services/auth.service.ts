1c0dfc1ae72d44fa2b79ed14929f288bac3bbb0a
la la <13687739+daedalus1215@users.noreply.github.com>
2025-02-07 14:51:10 -0500

    notes: begin to wire in aggregator to auth service

---
+++ src/auth/domain/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcrypt";
+import { UserAggregator } from "src/users/domain/aggregators/user.aggregator";
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userAggregator: UserAggregator,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userAggregator.findByEmail(username);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // If the user is found and the password matches, return the user object without the password
+      // src/auth/auth.service.ts
+      const { password, ...result } = user;
+      return result; // Return the user without the password
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
