2f16c178d2779b51f805a4b7977ebe0130464ec6
la la <13687739+daedalus1215@users.noreply.github.com>
2025-02-07 14:50:56 -0500

    notes: begin to work on aggregator

--- src/users/app/controllers/users.controller.ts
+++ src/users/app/controllers/users.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from '../../domain/users.service';
 import { CreateUserDto } from '../dtos/create-user.dto';
-import { JwtAuthGuard } from '../../../auth/jwt-auth.guard';
+import { JwtAuthGuard } from '../../../auth/app/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
