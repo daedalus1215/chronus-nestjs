8373feee245c0d7f31d4cdcde1c4d92406c05fd4
la la <13687739+daedalus1215@users.noreply.github.com>
2025-02-08 07:41:46 -0500

    notes: wire in log user in and user aggregator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { logoutAction } from './app/actions/logout.action';
 import { JwtStrategy } from './app/strategies/jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { LogUserIn } from './app/actions/log-user-in.action';
+import { AuthService } from './domain/services/auth.service';
 
 @Module({
     imports: [
         ConfigModule,
         PassportModule,
     ],
-    providers: [JwtStrategy],
-    controllers: [logoutAction],
+    providers: [JwtStrategy, AuthService],
+    controllers: [logoutAction, LogUserIn],
     exports: [PassportModule],
 })
 export class AuthModule { }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersService } from "./domain/users.service";
 import { UsersController } from "./app/controllers/users.controller";
 import { User } from "./domain/entities/user.entity"; // Adjust import based on your new entity
+import { UserAggregator } from "./domain/aggregators/user.aggregator";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
   ],
   controllers: [UsersController],
-  exports: [UsersService],
-  providers: [UsersService],
+  exports: [UserAggregator],
+  providers: [UsersService, UserAggregator],
 })
 export class UsersModule {}
